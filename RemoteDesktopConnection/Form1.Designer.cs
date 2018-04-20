65cbf6550403b19190cf2ba0a595ee7401d43b0c
bizimunda <yourbelover@gmail.com>
2018-04-20 13:38:26 +0200

    First comment

---
+++ RemoteDesktopConnection/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace RemoteDesktopConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblServer = new System.Windows.Forms.Label();
+            this.tbServer = new System.Windows.Forms.TextBox();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.rdp = new AxMSTSCLib.AxMsTscAxNotSafeForScripting();
+            ((System.ComponentModel.ISupportInitialize)(this.rdp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Location = new System.Drawing.Point(15, 908);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(50, 17);
+            this.lblServer.TabIndex = 0;
+            this.lblServer.Text = "Server";
+            // 
+            // tbServer
+            // 
+            this.tbServer.Location = new System.Drawing.Point(74, 905);
+            this.tbServer.Name = "tbServer";
+            this.tbServer.Size = new System.Drawing.Size(126, 22);
+            this.tbServer.TabIndex = 1;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(296, 908);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(100, 22);
+            this.tbUsername.TabIndex = 3;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(206, 911);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(77, 17);
+            this.lblUserName.TabIndex = 2;
+            this.lblUserName.Text = "User name";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(515, 910);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(100, 22);
+            this.tbPassword.TabIndex = 5;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(425, 913);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(69, 17);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(691, 908);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(822, 909);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(124, 32);
+            this.btnDisconnect.TabIndex = 7;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // rdp
+            // 
+            this.rdp.Enabled = true;
+            this.rdp.Location = new System.Drawing.Point(18, 41);
+            this.rdp.Name = "rdp";
+            this.rdp.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("rdp.OcxState")));
+            this.rdp.Size = new System.Drawing.Size(1095, 642);
+            this.rdp.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1382, 953);
+            this.Controls.Add(this.rdp);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.tbServer);
+            this.Controls.Add(this.lblServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.rdp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.TextBox tbServer;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private AxMSTSCLib.AxMsTscAxNotSafeForScripting rdp;
+    }
+}
+
---
+++ RemoteDesktopConnection/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MSTSCLib;
+
+namespace RemoteDesktopConnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                rdp.Server = tbServer.Text;
+                rdp.UserName = tbUsername.Text;
+                IMsTscNonScriptable secured = (IMsTscNonScriptable)rdp.GetOcx();
+                secured.ClearTextPassword = tbPassword.Text;
+                rdp.Connect();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong" + ex.StackTrace,"Click on ok"+ MessageBoxButtons.OK);   
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (rdp.Connected.ToString() == "1")
+                {
+                    rdp.Disconnect();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("You are not connected" + ex.StackTrace, "Click on ok" + MessageBoxButtons.OK);   
+            }
+        }
+    }
+}
